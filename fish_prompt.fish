4d726e5aaa7aa2eb8dbb746eb1d3cd10b3934320
Ryan Christopher Sanna <rsann@rypro.local>
2020-04-07 12:10:51 -0700

    k8s ctx in prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -4,7 +4,28 @@ function _is_git_dirty
   echo (command git status -s --ignore-submodules=dirty 2> /dev/null)
 end
 
+function kubectl_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo (set_color green)$KUBECTL_PROMPT_ICON" "(set_color purple)"$ctx$KUBECTL_PROMPT_SEPARATOR$ns"
+end
+
 function fish_prompt
+  # line 1
   set_color -o red
   printf '┌─<'
   set_color -o blue
@@ -20,6 +41,7 @@ function fish_prompt
   set_color -o red
   printf '>'
 
+  # line 2
   echo
   set_color -o red
   printf '└─<'
@@ -29,6 +51,16 @@ function fish_prompt
     set_color blue
     printf '* '
   end
+
+  set -l k8s (kubectl_status)
+  if [ ! -z "$k8s" ]
+    set_color -o purple
+    printf '['
+    printf '%s' (kubectl_status)
+    set_color -o purple
+    printf '] '
+  end
+
   set_color -o red
   printf '>──'
   set_color yellow
