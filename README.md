b345ee0429dc4c3a8b196dbc20ed82d4395dffd0
Ryan Christopher Sanna <rsann@rypro.local>
2020-04-07 10:05:19 -0700

    zish-k8s initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Zish-k8s
+
+Fish zish theme modified to include kubectl information
+
+![zish-k8s](./zish_k8s_preview.png)
+
+## Left prompt
+User, host, abbreviated path, and git branch info
+
+## Right prompt
+Kubectl status & exit code / time
+
+Enjoy!
---
+++ fish_right_prompt.fish
@@ -0,0 +1,41 @@
+function kubectl_status
+  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
+  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
+  set -l config $KUBECONFIG
+  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
+  if [ ! -f $config ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+    return
+  end
+
+  set -l ctx (kubectl config current-context 2>/dev/null)
+  if [ $status -ne 0 ]
+    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
+    return
+  end
+
+  set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$ctx\")].context.namespace}")
+  [ -z $ns ]; and set -l ns 'default'
+
+  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
+end
+
+function exit_code
+  set -l exit_code $status
+ 
+  if test $exit_code -ne 0
+    set_color red
+  else
+    set_color green
+  end
+ 
+  printf '%d' $exit_code
+  set_color yellow
+ 
+  printf ' < %s' (date +%H:%M:%S)
+  set_color normal
+end
+
+function fish_right_prompt
+  echo (kubectl_status)(exit_code)
+end
